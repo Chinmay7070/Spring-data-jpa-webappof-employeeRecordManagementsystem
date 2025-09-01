55cb16d1c1c5ac3a3329ea97a06627306dd47136
Chinmay Chaudhari <chaudharichinmay227@gmail.com>
2025-09-01 09:30:14 +0530

    implement soft deletiion

--- Springboot-data-jpa-web-emp-crud-project/src/main/java/com/nt/model/Employee.java
+++ Springboot-data-jpa-web-emp-crud-project/src/main/java/com/nt/model/Employee.java
@@ -2,6 +2,9 @@ package com.nt.model;
 
 import java.io.Serializable;
 
+import org.hibernate.annotations.SQLDelete;
+import org.hibernate.annotations.Where;
+
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
@@ -12,16 +15,20 @@ import lombok.Data;
 
 @Data
 @Entity
-@Table(name="emp")
-public class Employee implements Serializable{
-	
-	
-	@Id
-	@SequenceGenerator(name="gen1",sequenceName="emp_seq",initialValue=1,allocationSize=1)
-	@GeneratedValue(generator="gen1",strategy=GenerationType.SEQUENCE)
-	private Integer empno;
-	private String ename;
-	private String job="clrk";
-	private Double sal;
+//@Table(name="emp")
+@Table(name = "boot_emp")
+@SQLDelete(sql = "UPDATE boot_emp SET status='deleted' WHERE empno=?")
+@Where(clause = "status <> 'deleted'")
+public class Employee implements Serializable {
+
+    @Id
+    @SequenceGenerator(name = "gen1", sequenceName = "emp_seq", initialValue = 1, allocationSize = 1)
+    @GeneratedValue(generator = "gen1", strategy = GenerationType.SEQUENCE)
+    private Integer empno;
+
+    private String ename;
+    private String job = "clrk";
+    private Double sal;
+    private String status = "active";
 
 }
